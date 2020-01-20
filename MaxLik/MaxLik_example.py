653abac4c096c88d4b69ca205e370313f4e98fea
RobStarek <60106783+RobStarek@users.noreply.github.com>
2020-01-20 17:15:29 +0100

    Update MaxLik_example.py
    Minor changes to adjust names and comments.

--- MaxLik/MaxLik_example.py
+++ MaxLik/MaxLik_example.py
@@ -1,10 +1,10 @@
 """
-Minimal example how to use MaxLikCore.
+Minimal example how to use MaxLik.py.
 """
 
 import numpy as np
-from MaxLikCore import MakeRPV, estim
-#Definition of projection vector
+from MaxLik import MakeRPV, Reconstruct
+#Definition of projection vector (Pauli matrices eigenstate tomography)
 LO = np.array([[1],[0]])
 HI = np.array([[0],[1]])
 Plus = (LO+HI)*(2**-.5)
@@ -12,11 +12,7 @@ Minus = (LO-HI)*(2**-.5)
 RPlu = (LO+1j*HI)*(2**-.5)
 RMin = (LO-1j*HI)*(2**-.5)
 
-#Definion of measurement order, matching data order
-Order = [[LO,HI,Plus,Minus,RPlu,RMin]]
-#Measured counts, correcponding to |0>+1j|1> state.
-testdata = np.array([500,500,500,500,1000,1])
-#Prepare (Rho)-Pi vect
-RPV = MakeRPV(Order, False)
-#Run reconstruction
-E = estim(testdata, RPV, 1000, 1e-6)
+Order = [[LO,HI,Plus,Minus,RPlu,RMin]] #Definion of measurement order, matching data order
+testdata = np.array([500,500,500,500,1000,1]) #Measured counts, correcponding to |0>+1j|1> state.
+RPV = MakeRPV(Order, False) #Prepare (Rho)-Pi vect
+E = Reconstruct(testdata, RPV, 1000, 1e-6) #Run reconstruction
