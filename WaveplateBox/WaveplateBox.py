fcb93af80d9ca0bb64f517aac9cdd5095711e05d
RobStarek <60106783+RobStarek@users.noreply.github.com>
2020-05-28 14:48:00 +0200

    Correct version of the code.

--- WaveplateBox/WaveplateBox.py
+++ WaveplateBox/WaveplateBox.py
@@ -8,7 +8,7 @@ Jones formalism and Bloch-sphere representation is used.
 
 def ROT(x):
     """
-    Matrix of 2D coordinate rotation of angle x.    
+    Matrix of 2D coordinate rotation with angle x.    
     """
     cx = np.cos(x)
     sx = np.sin(x)
@@ -30,7 +30,16 @@ def QWP(x):
     R = ROT(x)
     return R @ M @ R.T
 
-def WPPrepare(x,y, input_state = np.array([[1],[0]])):
+def WP(x, gamma):
+    """
+    Rotated gamma-wave plate in Jones formalism.
+    """    
+    M = np.array([[1,0],[0,np.exp(-1j*gamma)]])
+    R = ROT(x)
+    return R @ M @ R.T
+
+
+def WPPrepare(x,y, input_state = np.array([[1],[0]]), dret1=0, dret2=0):
     """
     Jones vector (ket) of polarization prepared from input_state through a 
     pair of wave plates.
@@ -39,10 +48,31 @@ def WPPrepare(x,y, input_state = np.array([[1],[0]])):
         x - rotation of half-wave plate
         y - rotation of quarter-wave plate
         input_state - Jones (ket) vector of input state
+        dret1 - retardance error of half-wave plate
+        dret2 - retardance error of quarter-wave plate
     Returns:
         Jones vector of the prepared state
     """
-    return HWP(x) @ QWP(y) @ input_state
+    if dret1!=0 and dret2!=0:
+        return WP(x, np.pi + dret1) @ WP(y, np.pi/2 + dret2) @ input_state
+    else:
+        return HWP(x) @ QWP(y) @ input_state
+
+
+def WPProj(x,y, proj_state = np.array([[1],[0]]), dret1=0, dret2=0):   
+    """
+    Waveplate projector state. ->HWP(x)->QWP(y)->POL
+    Args:
+        x ... rotation angle of half-wave plate (rad)
+        y ... rotation angle of quarter-wave plate
+        proj_state ... used polarization filter, its ket representation
+        dret1 ... retardance of HWP
+        dret2 ... retardance of QWP
+    Returns:
+        <bra| vector of projecting state
+    """ 
+    return (proj_state.T.conjugate()) @ WP(y, np.pi/2 + dret2) @ WP(x, np.pi + dret1)
+
 
 def QHQBlock(x,y,z):
     """
@@ -92,6 +122,7 @@ def SearchForU(U, tol=1e-6):
     Search for angles x,y,z which implement desired unitary U with
     three wave-plates:
     QWP(X)-HWP(y)-QWP(z)->    
+    In this version, only perfect waveplates are considered.
     Args:
         U - desired unitary single-qubit operation
         tol - minimizer tolerance, see scipy.optimize.minimize docs.
@@ -109,7 +140,7 @@ def SearchForU(U, tol=1e-6):
     idx = np.argmin(Fs)    
     return Rs[idx]
 
-def SearchForKet(ket, input_state=np.array([[1],[0]]), tol=1e-6):
+def SearchForKet(ket, input_state=np.array([[1],[0]]), dret1 = 0, dret2 = 2, tol=1e-6):
     """
     Search for wave plates angles x,y which prepare desired ket state from input state
     with setup:
@@ -117,6 +148,8 @@ def SearchForKet(ket, input_state=np.array([[1],[0]]), tol=1e-6):
     Args:
         ket - desired Jones vector to be prepared
         input_state - input Jones vector
+        dret1 - retardance error of half-wave plate
+        dret2 - retardance error of quarter-wave plate
         tol - minimizer tolerance, see scipy.optimize.minimize docs.
     Returns:
         R - dictionary with minimization details. 
@@ -125,7 +158,7 @@ def SearchForKet(ket, input_state=np.array([[1],[0]]), tol=1e-6):
     """    
     #Construct function to be minimized
     def minim(x):
-        ketX = WPPrepare(x[0],x[1], input_state)        
+        ketX = WPPrepare(x[0],x[1], input_state, dret1, dret2)
         return -np.abs(ketX.T.conjugate() @ ket)[0,0]**2
     #Start minimization from multiple initial guessses to avoid sub-optimal local extremes.
     Rs = [minimize(minim, g, bounds=[wp_bounds]*2, tol=tol) for g in search_grid_qh]
@@ -134,7 +167,7 @@ def SearchForKet(ket, input_state=np.array([[1],[0]]), tol=1e-6):
     idx = np.argmin(Fs)
     return Rs[idx]
 
-def SearchForRho(rho, input_state=np.array([[1],[0]]), tol=1e-6):
+def SearchForRho(rho, input_state=np.array([[1],[0]]), dret1 = 0, dret2 = 0, tol=1e-6):
     """
     Search for wave plates angles x,y which prepare desired pure density matrix from input state
     with setup:
@@ -142,6 +175,8 @@ def SearchForRho(rho, input_state=np.array([[1],[0]]), tol=1e-6):
     Args:
         ket - desired Jones vector to be prepared
         input_state - input Jones vector
+        dret1 - retardance error of half-wave plate
+        dret2 - retardance error of quarter-wave plate
         tol - minimizer tolerance, see scipy.optimize.minimize docs.
     Returns:
         R - dictionary with minimization details. 
@@ -150,8 +185,36 @@ def SearchForRho(rho, input_state=np.array([[1],[0]]), tol=1e-6):
     """    
     #Construct function to be minimized
     def minim(x):
-        ketX = WPPrepare(x[0],x[1], input_state)        
+        ketX = WPPrepare(x[0],x[1], input_state, dret1, dret2)        
         return -np.abs(ketX.T.conjugate() @ rho @ ketX)[0,0]
+    #Start minimization from multiple initial guessses to avoid sub-optimal local extremes.
+    Rs = [minimize(minim, g, bounds=[wp_bounds]*2, tol=tol) for g in search_grid_qh]
+    #Pick global extereme.
+    Fs = np.array([R['fun'] for R in Rs])
+    idx = np.argmin(Fs)
+    return Rs[idx]
+
+def SearchForProj(ket, projector_state=np.array([[1],[0]]), dret1 = 0, dret2 = 2, tol=1e-6):
+    """
+    Search for wave plates angles x,y which makes projection onto desired
+    ket state with given projector state with setup:
+    input_state->QWP(y)-HWP(x)->
+    --HWP(x)-QWP(y)->projector
+    Args:
+        ket - desired Jones vector to be projected on
+        input_state - polarizers eigenstate
+        dret1 - retardance error of half-wave plate
+        dret2 - retardance error of quarter-wave plate
+        tol - minimizer tolerance, see scipy.optimize.minimize docs.
+    Returns:
+        R - dictionary with minimization details. 
+        R['x'] contains desired angles, R['fun'] is measure of quality (should be -1)
+        See scipy.optimize.minimize docs for more details.
+    """    
+    #Construct function to be minimized
+    def minim(x):
+        bra = WPProj(x[0],x[1], np.array([[1],[0]]), 0, 0) 
+        return -np.abs(bra @ ket)[0,0]**2
     #Start minimization from multiple initial guessses to avoid sub-optimal local extremes.
     Rs = [minimize(minim, g, bounds=[wp_bounds]*2, tol=tol) for g in search_grid_qh]
     #Pick global extereme.
